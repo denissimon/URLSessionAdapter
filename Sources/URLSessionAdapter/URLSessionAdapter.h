7f1e3b6cc20e55c001f43d5b4b8ec781df5f65a6
Denis <denis.v.simon@gmail.com>
2024-01-24 12:13:05 +0500

    Added Carthage support

---
+++ Sources/URLSessionAdapter/URLSessionAdapter.h
@@ -0,0 +1,18 @@
+//
+//  URLSessionAdapter.h
+//  https://github.com/denissimon/URLSessionAdapter
+//
+//  Created by Denis Simon on 24/01/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for URLSessionAdapter.
+FOUNDATION_EXPORT double URLSessionAdapterVersionNumber;
+
+//! Project version string for URLSessionAdapter.
+FOUNDATION_EXPORT const unsigned char URLSessionAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <URLSessionAdapter/PublicHeader.h>
+
+
