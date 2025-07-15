ebb9077080080693661928b1dd752afdfb5b33d2
Denis <denis.v.simon@gmail.com>
2025-07-15 15:51:58 +0300

    Add ProgressObserver.swift

---
+++ Sources/URLSessionAdapter/ProgressObserver.swift
@@ -0,0 +1,26 @@
+//
+//  ProgressObserver.swift
+//  https://github.com/denissimon/URLSessionAdapter
+//
+//  Created by Denis Simon on 09/07/2025.
+//
+//  MIT License (https://github.com/denissimon/URLSessionAdapter/blob/main/LICENSE)
+//
+
+import Foundation
+
+public class ProgressObserver: NSObject, URLSessionDataDelegate {
+    
+    public var observation: NSKeyValueObservation? = nil
+    public let onChangeHandler: (Progress) -> Void
+    
+    public init(onChangeHandler: @escaping (Progress) -> Void) {
+        self.onChangeHandler = onChangeHandler
+    }
+    
+    public func urlSession(_ session: URLSession, didCreateTask task: URLSessionTask) {
+        observation = task.progress.observe(\.fractionCompleted) { progress, change in
+            self.onChangeHandler(progress)
+        }
+    }
+}
