eff59bddd2ba6b7de31bbdf20536d1ad998461aa
Denis <denis.v.simon@gmail.com>
2025-07-16 19:01:25 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "URLSessionAdapter",
+    platforms: [.iOS(.v15), .macOS(.v12), .tvOS(.v15), .watchOS(.v8)],
     products: [
         .library(
             name: "URLSessionAdapter",
