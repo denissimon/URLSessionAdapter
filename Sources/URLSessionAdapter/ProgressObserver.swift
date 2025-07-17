3494add33d2f5c256e289b9e6ffb90af8ca7e519
Denis <denis.v.simon@gmail.com>
2025-07-17 17:02:20 +0300

    Update ProgressObserver.swift

--- Sources/URLSessionAdapter/ProgressObserver.swift
+++ Sources/URLSessionAdapter/ProgressObserver.swift
@@ -9,10 +9,10 @@
 
 import Foundation
 
-public class ProgressObserver: NSObject, URLSessionDataDelegate, @unchecked Sendable {
+open class ProgressObserver: NSObject, URLSessionDataDelegate, @unchecked Sendable {
     
-    private var observation: NSKeyValueObservation? = nil
-    private let onChangeHandler: (Progress) -> Void
+    var observation: NSKeyValueObservation? = nil
+    let onChangeHandler: (Progress) -> Void
     
     public init(onChangeHandler: @escaping (Progress) -> Void) {
         self.onChangeHandler = onChangeHandler
